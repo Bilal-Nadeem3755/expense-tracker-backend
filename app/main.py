bf9f1a3cc501007443aa668754b9fcd948d5746e
Bilal-Nadeem3755 <malikbilal5573@gmail.com>
2026-09-08 17:03:34 +0500

    fix: allow production frontend CORS

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-    ],
+        "https://expense-tracker-frontend-six-ochre.vercel.app",
+     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
